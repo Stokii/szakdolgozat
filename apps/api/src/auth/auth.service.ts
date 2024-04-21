9094672c7411585c1d36da1ffb721778e1184efa
Tóth Dániel <tothdani561@gmail.com>
2024-04-21 16:30:44 +0200

    Make access token
    signToken method

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -3,10 +3,12 @@ import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class authService {
-    constructor(private prisma: PrismaService) {}
+    constructor(private prisma: PrismaService, private jwt: JwtService, private config: ConfigService) {}
     async register(dto: AuthDto) {
         // Legeneráljuk a jelszó hash-t
         const hash = await argon.hash(dto.password);
@@ -16,10 +18,9 @@ export class authService {
         const user = await this.prisma.user.create({
             data: { email: dto.email, hash, firstName: 'Toth',lastName: 'Daniel'}
         });
-        delete user.hash;
 
         // visszatérünk a mentett felhasználóval
-        return user;
+        return this.signToken(user.id, user.email);
     } catch(error){
         if (error instanceof PrismaClientKnownRequestError) {
             if (error.code === 'P2002') {
@@ -47,8 +48,21 @@ export class authService {
         if (!pwMatches) throw new ForbiddenException('Helytelen jelszó!')
 
         // visszaadjuk a user-t
-        delete user.hash;
-        return user;
+        return this.signToken(user.id, user.email);
+    }
+
+    // Itt állítjuk össze a tokent
+    async signToken(userId: number, email: string): Promise<{access_token: string}> {
+        const payload = {
+            sub: userId,
+            email,
+        }
+
+        // 15 percig tud a felhasználó tevékenykedni, aztán kidobja a rendszer
+        const secret = this.config.get('JWT_SECRET');
+        const token = await this.jwt.signAsync(payload, { expiresIn: '15m', secret: secret})
+
+        return {access_token: token};
     }
 
     getHello(): string {
--- node_modules/api/src/auth/auth.module.ts
+++ node_modules/api/src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { authController } from "./auth.controller";
 import { authService } from "./auth.service";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
+    imports: [JwtModule.register({})],
     controllers: [authController],
     providers: [authService]
 })
